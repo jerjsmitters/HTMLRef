3f09e89f538b09c061b25e42ad530fe422463363
George Smith <george.smith@netmatters.local>
2018-10-18 15:39:12 +0100

    About to make a big CSS reshuffle in order to introduce a new media query for the side-menu

--- javascript/main.js
+++ javascript/main.js
@@ -6,19 +6,30 @@ $("#email").click( () =>{
 
 //SideMenu
 let move = false;
+let mobMoved = $("#mob-menu").css("width");
+
 $("#burger").click( () => {
   if (!move){
-    $("#main-body").css("transform", "translateX(-35%)");
+    $("#main-body").css("transform", `translateX(-${mobMoved})`); //body to left
+    $("#mob-menu").css("transform", "translateX(0%)"); //menu to left
+    $("#sticky-chat").css("visibility", "hidden"); //sticky chat gone
+    $("#darken").css("visibility", "visible").css("opacity", "1").css("z-index", "500");
     move = true;
   }else {
     $("#main-body").css("transform", "translateX(0)");
+    $("#mob-menu").css("transform", "translateX(100%)");
+    $("#sticky-chat").css("visibility", "visible");
+    $("#darken").css("visible", "hidden").css("opacity", "0").css("z-index", "0");
     move = false;
   }
 });
 
-//establish positions of menu  COMPLETE
-//construct selector that selects all in body but #mob-menu COMPLETE
-//menu moves to left on burger click
+console.log(mobMoved)
+
+//Works when cardbox is of fixed width and
+
 //menu should then become sticky.
-//sticky chat should disappear
+
 //style menu
+
+//fix sticky menu
