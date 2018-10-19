ce1313e91e12d508b054f5c39bac64fb4c81c797
George Smith <george.smith@netmatters.local>
2018-10-19 12:49:44 +0100

    Mobile header complete. All that needs fixing is the glitch caused by the div being inserted into the DOM.

--- javascript/main.js
+++ javascript/main.js
@@ -8,30 +8,58 @@ $("#email").click( () =>{
 //SideMenu
 let move = false;
 let mobMoved = $("#mob-menu").css("width"); //width of mobile menu; used to determine
-                                            //how much body will move upon slide. 
+                                            //how much body will move upon slide.
 
 $("#burger").click( () => {
+  //Problems:
+  //If burger is clicked and then unclicked, menu becomes unstuck.
+  //If burger is clicked and then scrolling occurs, menu becomes unstuck.
+
+  //THE CODE BELOW IS FINE
   if (!move){
     $("#main-body").css("transform", `translateX(-${mobMoved})`); //body to left
-    $("#mob-menu").css("transform", "translateX(0%)"); //menu to left
+    $("#mob-menu").css("transform", "translateX(0%)"); //side-menu to left
     $("#sticky-chat").css("visibility", "hidden"); //sticky chat gone
-    $("#darken").css("visibility", "visible").css("opacity", "1").css("z-index", "500");
+    $("#darken").css("visibility", "visible").css("opacity", "1").css("z-index", "500"); //darken effect
     move = true;
   }else {
     $("#main-body").css("transform", "translateX(0)");
     $("#mob-menu").css("transform", "translateX(100%)");
     $("#sticky-chat").css("visibility", "visible");
-    $("#darken").css("visible", "hidden").css("opacity", "0").css("z-index", "0");
+    $("#darken").css("visibility", "hidden").css("opacity", "0").css("z-index", "0");
     move = false;
   }
 });
 
 
+//Sticky nav bar mobile
+let fillVar = true;
 
-//Works when cardbox is of fixed width and
+$(document).scroll(()=>{
+  let scrollAmount = $(document).scrollTop();
+  if (scrollAmount > 0) {
+    $("#stickyboi").addClass("sticky-on");
 
-//menu should then become sticky.
+    if (fillVar) { //filVar FALSE: STICK, TRUE: NOT STUCK
+      $('#logo').addClass('sticky-logo').removeAttr('id');
+      $('.call').addClass('sticky-call').removeClass('call');
+      $("body").prepend("<div id='filler'></div>");
+      $('.header').css("backgroundImage", "linear-gradient(#0ce5ff, #06cbe2, #0099ab)")
+                  .css('height', '92px');
+      $('#burger').addClass('sticky-burger').removeAttr('id');
+      fillVar = false; //So filler is only inserted once
+    }
 
-//style menu
+  } else {
+    $("#stickyboi").removeClass("sticky-on");
+    $('.sticky-logo').attr('id', 'logo').removeClass('sticky-logo');
+    $('.sticky-call').addClass('call').removeClass('sticky-call');
+    $('.header').css("backgroundImage", "none")
+                .css('height', 'auto');
+    $('.sticky-burger').attr('id', 'burger').removeClass('sticky-burger');
+    $("#filler").remove();
 
-//fix sticky menu
+    fillVar=true;
+  }
+
+});
