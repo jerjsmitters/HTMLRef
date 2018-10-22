212dfe78757244de7d47c14f97f854873f0f5cde
George Smith <george.smith@netmatters.local>
2018-10-22 09:49:35 +0100

    Mobile menu is complete with all desired features

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,20 @@
-//SignUp Button
-$("#email").click( () =>{
-  $("#sign-up").slideDown("fast", "swing");
-  $("#email").css("marginBottom", "0px");
-});
-
-
 //SideMenu
 let move = false;
 let mobMoved = $("#mob-menu").css("width"); //width of mobile menu; used to determine
                                             //how much body will move upon slide.
 
 $("#burger").click( () => {
-  //Problems:
-  //If burger is clicked and then unclicked, menu becomes unstuck.
-  //If burger is clicked and then scrolling occurs, menu becomes unstuck.
-  //Around 936px, if the menu becomes sticky, the cards becomes grossly misaligned with the rest of the document.
-
-  //THE CODE BELOW IS FINE
   if (!move){
-    $("#main-body").css("transform", `translateX(-${mobMoved})`); //body to left
-    $("#mob-menu").css("transform", "translateX(0%)"); //side-menu to left
+    $("html").css("overflow", "hidden");
+    $("#main-body").css("right", `${mobMoved}`).css('overflow', 'hidden'); //body to left
+    $("#mob-menu").css("right", "0").css("top", "0"); //side-menu to left
     $("#sticky-chat").css("visibility", "hidden"); //sticky chat gone
     $("#darken").css("visibility", "visible").css("opacity", "1").css("z-index", "500"); //darken effect
     move = true;
   }else {
-    $("#main-body").css("transform", "translateX(0)");
-    $("#mob-menu").css("transform", "translateX(100%)");
+    $("html").css("overflow", "visible");
+    $("#main-body").css("right", "0").css('overflow', 'visible');
+    $("#mob-menu").css("right", `-${mobMoved}`);
     $("#sticky-chat").css("visibility", "visible");
     $("#darken").css("visibility", "hidden").css("opacity", "0").css("z-index", "0");
     move = false;
@@ -44,19 +33,19 @@ $(document).scroll(()=>{
       $('#logo').addClass('sticky-logo').removeAttr('id');
       $('.call').addClass('sticky-call').removeClass('call');
       $("body").prepend("<div id='filler'></div>");
+      $('#filler').css("height","200px")
       $('.header').css("backgroundImage", "linear-gradient(#0ce5ff, #06cbe2, #0099ab)")
                   .css('height', '92px');
       $('#burger').addClass('sticky-burger').removeAttr('id');
-      $('.card-container').addClass('card-container-fix');
+      $('.card-container').addClass('card-container-fix'); //fixes misalignment issue
       fillVar = false; //So filler is only inserted once
     }
-
   } else { //STUCK->UNSTUCK
     $("#stickyboi").removeClass("sticky-on");
     $('.sticky-logo').attr('id', 'logo').removeClass('sticky-logo');
     $('.sticky-call').addClass('call').removeClass('sticky-call');
     $('.header').css("backgroundImage", "none")
-                .css('height', 'auto');
+                .css('height', '190px');
     $('.sticky-burger').attr('id', 'burger').removeClass('sticky-burger');
     $("#filler").remove();
     $('.card-container').removeClass('card-container-fix');
